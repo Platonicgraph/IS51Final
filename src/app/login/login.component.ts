d26690187c54b8abfdd01369d47ec5a90d193508
samuel day <samgrd.mail.fresnostate.edu>
2019-05-15 19:39:56 -0700

    exam progress saved

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -2,6 +2,13 @@ import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { ToastService } from '../toast/toast.service';
 import { Subject } from 'rxjs';
+
+export interface IUser {
+  id?: number;
+  username: string;
+  password: string;
+}
+
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
@@ -9,11 +16,36 @@ import { Subject } from 'rxjs';
 })
 export class LoginComponent implements OnInit {
 
+  user: IUser = {
+    username: null,
+    password: null
+  };
+
   constructor(private router: Router, private toastService: ToastService) {
   }
 
   ngOnInit() {
 
+  }
+
+  login(user: IUser) {
+    const presetUser = { username: 'samuel', password: 'samuel123' };
+    if (user.username != null && user.password != null &&
+      user.username !== '' && user.password !== '') {
+      // log the user in
+      if (user.username === presetUser.username &&
+        user.password === presetUser.password) {
+        // actually log them in
+        // saving data to localStorage
+        localStorage.setItem('user', JSON.stringify(user));
+        // navigate to contact page
+        this.router.navigate(['cart', user]);
+      } else {
+        this.toastService.showToast('danger', 2000, 'Invalid credentials!');
+      }
+    } else {
+      this.toastService.showToast('danger', 2000, 'Must specify credentials');
+    }
   }
 
 }
